3c0f8f0b7f918af2f1955ef19ae9a47e0d183a30
Jhontona <jhontona@outlook.com>
2018-09-18 20:24:08 -0500

    TypeScript

---
+++ ecommerce-simpleUML2TypeScript/ecommerce/src/Stock.ts
@@ -0,0 +1,21 @@
+
+/* Generated with GenMyModel */
+
+/// <reference path="Product.ts" />
+
+    
+class Stock
+{
+    // Attributes
+	name: string;
+	address: string;
+	product: Product;
+	
+    // Implemented attributes
+	
+    // Operations
+	 
+	// Implemented operations
+	
+}
+
